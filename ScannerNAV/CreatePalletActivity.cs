b7aba6839722f1b02a6d16c1ab8bf4cdbfc51568
delahandschue <jra@relateit.dk>
2017-12-13 11:26:52 +0100

    Added CreatePallet

--- ScannerNAV/CreatePalletActivity.cs
+++ ScannerNAV/CreatePalletActivity.cs
@@ -1,27 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 using Android.App;
 using Android.Content;
 using Android.OS;
-using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using ScannerNAV.Webservice;
+
+
 
 namespace ScannerNAV
 {
-    [Activity(Label = "ScannerNAV", ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
+    [Activity(Label = "Create Pallet", ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
     public class CreatePalletActivity : Activity
     {
+        private Button btnCreate;
+        private EditText etPalletNo;
+        private TextView tvStatus;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             RequestWindowFeature(WindowFeatures.NoTitle);
 
             // Create your application here
-            Helper.ShowAlertDialog(this, "Wait for it..", "Not Implemented");
+            SetContentView(Resource.Layout.CreatePallet);
+
+            btnCreate = FindViewById<Button>(Resource.Id.btnCreate);
+            btnCreate.Click += OnCreateClick;
+
+            tvStatus = FindViewById<TextView>(Resource.Id.tvStatus);
+            tvStatus.Click += OnStatusClick;
+            tvStatus.Text = Helper.GetStatus();
+        }
+
+        private void OnCreateClick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnStatusClick(object sender, EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(SettingsActivity));
+            StartActivityForResult(intent, 0);
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            tvStatus.Text = Helper.GetStatus();
         }
     }
 }
